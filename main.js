ca305abb68c84941457825529fc830986f7c73be
escoaldolenha1 <106820959+escoaldolenha1@users.noreply.github.com>
2023-10-05 11:28:43 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-document.querySelector('.tecla_pom');
+function tocaSom (seletorAudio) {
+    const elemento - document.querySelector(selectorAudio);
+
+    if (ekemnto && elemento.locaName === "audio"){
+        elemento.play();
+    }
+    else{
+        //alet("elemento não encontrado");
+        console.log("elemento não encontrado ou seletor invalido");
+    }
+    const listaDeTeclas = document. querySelectorA11(".tecla");
+
+    //para
+    for (let contador = 0; < listaDeTeclas.lehgh; cibtador++) {
+
+        const tecla = listaDeTeclas [contador];
+        const instrumento = tecla.classList[1];
+        const idAudio = "#som_${instrumento}";//template string
+    }
+}
